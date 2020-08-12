8d1c11f2632ba7627c855e4c32449817043335f2
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2020-08-12 16:38:11 +0200

    Minor

--- src/mojibake.h
+++ src/mojibake.h
@@ -11,6 +11,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define MB_VERSION "1.0.0"
+#define MB_VERSION_NUMBER 0x100 /* MAJOR << 8 && MINOR << 4 && REVISION */
+#define MB_VERSION_MAJOR 1
+#define MB_VERSION_MINOR 0
+#define MB_VERSION_REVISION 0
+
+#define MB_UNICODE_VERSION "13.0"
+#define MB_UNICODE_VERSION_MAJOR 13
+#define MB_UNICODE_VERSION_MINOR 0
+
 /*
  A unicode codepoint
  [see: https://www.unicode.org/glossary/#code_point]
