90525dfa945211e7257f843882df9818b2a204e5
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2024-10-03 07:29:00 +0200

    Add UTF-8 tests generator

---
+++ utils/generate-utf8-tests.ts
@@ -0,0 +1,28 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { substituteText } from './utils';
+
+function generateTest(value: [number, string]) {
+  let hex = value[0].toString(16).padStart(4, '0').toUpperCase();
+  const char = new TextEncoder().encode(String.fromCodePoint(value[0]));
+  let escaped = Array.from(char).map(byte => `\\x${byte.toString(16).toUpperCase()}`).join('');
+
+  return `    TEST_UTF8(0x${hex}, "${escaped}", "${value[1]}");`;
+}
+
+export function generateEncodingTest() {
+  let fileContent = readFileSync('../tests/encoding.c', 'utf-8');
+
+  const tests: [number, string][] = [
+    [0x007F, 'ASCII limit'],
+    [0x7FF, '2-bytes limit'],
+    [0xFFFD, '3-bytes limit'],
+    [0x10FFFE, '4-bytes limit'],
+    [0x1F642, 'SLIGHTLY SMILING FACE'],
+  ];
+
+  fileContent = substituteText(fileContent, "// UTF-8 tests\n", "\n    return NULL;", tests.map(generateTest).join("\n") + "\n");
+
+  writeFileSync('../tests/encoding.c', fileContent);
+}
+
+generateEncodingTest();
