7ddf28e8065008eac0107b52dba2d60a3da1f0c7
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-06-14 09:35:16 +0200

    Minor fixes to tests

--- tests/version.c
+++ tests/version.c
@@ -8,10 +8,9 @@
 #include "test.h"
 
 void *test_version(void *arg) {
-    ATT_ASSERT(strcmp(mjb_version(), MJB_VERSION), 0, "Valid version");
+    ATT_ASSERT(mjb_version(), MJB_VERSION, "Valid version");
     ATT_ASSERT(mjb_version_number(), MJB_VERSION_NUMBER, "Valid version number");
-    ATT_ASSERT(mjb_version_number(), MJB_VERSION_NUMBER, "Valid version number");
-    ATT_ASSERT(strcmp(mjb_unicode_version(), MJB_UNICODE_VERSION), 0, "Valid unicode version");
+    ATT_ASSERT(mjb_unicode_version(), MJB_UNICODE_VERSION, "Valid unicode version");
 
     return NULL;
 }
