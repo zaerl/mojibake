7fd5c4ad6e7fa769266ab8ae007e610dacc8d4c1
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-06-14 15:13:29 +0200

    Fix: outdated script

--- utils/generate-utf8-tests.ts
+++ utils/generate-utf8-tests.ts
@@ -4,9 +4,10 @@ import { substituteText } from './utils';
 function generateTest(value: [number, string]) {
   let hex = value[0].toString(16).padStart(4, '0').toUpperCase();
   const char = new TextEncoder().encode(String.fromCodePoint(value[0]));
-  let escaped = Array.from(char).map(byte => `\\x${byte.toString(16).toUpperCase()}`).join('');
+  const chars = Array.from(char);
+  let escaped = chars.map(byte => `\\x${byte.toString(16).toUpperCase()}`).join('');
 
-  return `    TEST_UTF8(0x${hex}, "${escaped}", "${value[1]}");`;
+  return `    TEST_UTF8(0x${hex}, "${escaped}", ${chars.length}, "${value[1]}");`;
 }
 
 export function generateEncodingTest() {
@@ -15,6 +16,7 @@ export function generateEncodingTest() {
   const tests: [number, string][] = [
     [0x007F, 'ASCII limit'],
     [0x7FF, '2-bytes limit'],
+    [0x1E0A, 'LATIN CAPITAL LETTER D WITH DOT ABOVE'],
     [0xFFFD, '3-bytes limit'],
     [0x10FFFE, '4-bytes limit'],
     [0x1F642, 'SLIGHTLY SMILING FACE'],
