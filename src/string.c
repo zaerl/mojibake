1dff47087e515106430c69caf1b557b83a9b4b9a
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-08-08 22:49:31 +0200

    Add UTF16 support to mjb_strnlen

--- src/string.c
+++ src/string.c
@@ -6,28 +6,47 @@
 
 #include "mojibake.h"
 #include "utf8.h"
+#include "utf16.h"
 
 /**
  * Return size of a string.
  */
 MJB_EXPORT size_t mjb_strnlen(const char *buffer, size_t max_length, mjb_encoding encoding) {
-    if(buffer == 0 || max_length == 0 || encoding != MJB_ENCODING_UTF_8) {
+    if(buffer == 0 || max_length == 0) {
         return 0;
     }
 
     uint8_t state = MJB_UTF8_ACCEPT;
     mjb_codepoint codepoint;
     size_t count = 0;
-    const char *current = buffer;
 
-    while(*current && (size_t)(current - buffer) < max_length) {
-        state = mjb_utf8_decode_step(state, *current, &codepoint);
+    if(encoding == MJB_ENCODING_UTF_8) {
+        const char *current = buffer;
 
-        if(state == MJB_UTF8_ACCEPT) {
-            ++count;
+        while(*current && (size_t)(current - buffer) < max_length) {
+            state = mjb_utf8_decode_step(state, *current, &codepoint);
+
+            if(state == MJB_UTF8_ACCEPT) {
+                ++count;
+            }
+
+            ++current;
         }
+    } else if(encoding == MJB_ENCODING_UTF_16_LE || encoding == MJB_ENCODING_UTF_16_BE) {
+        state = MJB_UTF16_ACCEPT;
+        const uint8_t *bytes = (const uint8_t *)buffer;
 
-        ++current;
+        for(size_t i = 0; i < max_length; i += 2) {
+            uint16_t unit = encoding == MJB_ENCODING_UTF_16_BE ?
+                (bytes[i] << 8) | bytes[i + 1] :
+                bytes[i] | (bytes[i + 1] << 8);
+
+            state = mjb_utf16_decode_step(state, unit, &codepoint);
+
+            if(state == MJB_UTF16_ACCEPT) {
+                ++count;
+            }
+        }
     }
 
     return count;
--- tests/string.c
+++ tests/string.c
@@ -28,8 +28,55 @@
      ATT_ASSERT(mjb_strnlen("Hèllõ ツ", 5, encoding), 4, "UTF-8 length: Hèllõ ツ")
      ATT_ASSERT(mjb_strnlen("こんにちは", 15, encoding), 5, "UTF-8 length: こんにちは")
      ATT_ASSERT(mjb_strnlen("Γειά σου", 15, encoding), 8, "UTF-8 length: Γειά σου")
-
      ATT_ASSERT(mjb_strnlen("Héllö", 1, encoding), 1, "UTF-8 length: Héllö (1 max value)")
+
+     encoding = MJB_ENCODING_UTF_16_LE;
+     const char utf16le_hello[] = "H\0e\0l\0l\0o\0";
+     ATT_ASSERT(mjb_strnlen(utf16le_hello, 10, encoding), 5, "UTF-16LE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello, 8, encoding), 4, "UTF-16LE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello, 6, encoding), 3, "UTF-16LE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello, 4, encoding), 2, "UTF-16LE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello, 2, encoding), 1, "UTF-16LE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello, 0, encoding), 0, "UTF-16LE length: Hello")
+     ATT_ASSERT(mjb_strnlen(NULL, 0, encoding), 0, "UTF-16LE length: NULL")
+
+     const char utf16le_hello_accents[] = "H\0\xE9\0l\0l\0\xF6\0";
+     ATT_ASSERT(mjb_strnlen(utf16le_hello_accents, 10, encoding), 5, "UTF-16LE length: Héllö")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello_accents, 8, encoding), 4, "UTF-16LE length: Héllö")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello_accents, 4, encoding), 2, "UTF-16LE length: Héllö")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello_accents, 0, encoding), 0, "UTF-16LE length: Héllö")
+     const char utf16le_hello_katakana[] = "H\0\xE8\0l\0l\0\xF5\0 \0\x30\x30";
+     ATT_ASSERT(mjb_strnlen(utf16le_hello_katakana, 14, encoding), 7, "UTF-16LE length: Hèllõ ツ")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello_katakana, 10, encoding), 5, "UTF-16LE length: Hèllõ ツ")
+     const char utf16le_konnichiwa[] = "\x93\x30\x93\x30\x6B\x30\x61\x30\x6F\x30";
+     ATT_ASSERT(mjb_strnlen(utf16le_konnichiwa, 10, encoding), 5, "UTF-16LE length: こんにちは")
+     const char utf16le_geia_sou[] = "\x93\x03\x65\x03\x69\x03\x3F\x03 \0\xC3\x03\x6F\x03\x75\x03";
+     ATT_ASSERT(mjb_strnlen(utf16le_geia_sou, 16, encoding), 8, "UTF-16LE length: Γειά σου")
+     ATT_ASSERT(mjb_strnlen(utf16le_hello_accents, 2, encoding), 1, "UTF-16LE length: Héllö (1 max value)")
+
+     encoding = MJB_ENCODING_UTF_16_BE;
+     const char utf16be_hello[] = "\0H\0e\0l\0l\0o";
+     ATT_ASSERT(mjb_strnlen(utf16be_hello, 10, encoding), 5, "UTF-16BE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello, 8, encoding), 4, "UTF-16BE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello, 6, encoding), 3, "UTF-16BE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello, 4, encoding), 2, "UTF-16BE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello, 2, encoding), 1, "UTF-16BE length: Hello")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello, 0, encoding), 0, "UTF-16BE length: Hello")
+     ATT_ASSERT(mjb_strnlen(NULL, 0, encoding), 0, "UTF-16BE length: NULL")
+
+     const char utf16be_hello_accents[] = "\0H\0\xE9\0l\0l\0\xF6";
+     ATT_ASSERT(mjb_strnlen(utf16be_hello_accents, 10, encoding), 5, "UTF-16BE length: Héllö")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello_accents, 8, encoding), 4, "UTF-16BE length: Héllö")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello_accents, 4, encoding), 2, "UTF-16BE length: Héllö")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello_accents, 0, encoding), 0, "UTF-16BE length: Héllö")
+     const char utf16be_hello_katakana[] = "\0H\0\xE8\0l\0l\0\xF5\0 \x30\x30";
+     ATT_ASSERT(mjb_strnlen(utf16be_hello_katakana, 14, encoding), 7, "UTF-16BE length: Hèllõ ツ")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello_katakana, 10, encoding), 5, "UTF-16BE length: Hèllõ ツ")
+     const char utf16be_konnichiwa[] = "\x30\x93\x30\x93\x30\x6B\x30\x61\x30\x6F";
+     ATT_ASSERT(mjb_strnlen(utf16be_konnichiwa, 10, encoding), 5, "UTF-16BE length: こんにちは")
+     const char utf16be_geia_sou[] = "\x03\x93\x03\x65\x03\x69\x03\x3F\0 \x03\xC3\x03\x6F\x03\x75";
+     ATT_ASSERT(mjb_strnlen(utf16be_geia_sou, 16, encoding), 8, "UTF-16BE length: Γειά σου")
+     ATT_ASSERT(mjb_strnlen(utf16be_hello_accents, 2, encoding), 1, "UTF-16BE length: Héllö (1 max value)")
 
      return NULL;
  }
