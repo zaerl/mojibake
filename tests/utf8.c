69fd4e98e62b766cf00b1b10d53b406cb946f496
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-09-13 22:19:05 +0200

    Add UTF-16 tests

--- tests/utf8.c
+++ tests/utf8.c
@@ -11,9 +11,12 @@
 void *test_utf8(void *arg) {
     char buffer_utf8[5];
 
+    #define TEST_ENC(CHAR, STR, RES, COMMENT, ENCODING, BUFFER, BUFFER_SIZE) \
+        ATT_ASSERT(mjb_codepoint_encode(CHAR, (char*)BUFFER, BUFFER_SIZE, ENCODING), RES, COMMENT) \
+        ATT_ASSERT((const char*)BUFFER, STR, COMMENT)
+
     #define TEST_UTF8(CHAR, STR, RES, COMMENT) \
-        ATT_ASSERT(mjb_codepoint_encode(CHAR, (char*)buffer_utf8, 5, MJB_ENCODING_UTF_8), RES, COMMENT) \
-        ATT_ASSERT((const char*)buffer_utf8, STR, COMMENT)
+        TEST_ENC(CHAR, STR, RES, COMMENT, MJB_ENCODING_UTF_8, buffer_utf8, 5)
 
     // UTF-8 tests
     TEST_UTF8(0x007F, "\x7F", 1, "ASCII limit");
@@ -22,6 +25,28 @@ void *test_utf8(void *arg) {
     TEST_UTF8(0xFFFD, "\xEF\xBF\xBD", 3, "3-bytes limit");
     TEST_UTF8(0x10FFFE, "\xF4\x8F\xBF\xBE", 4, "4-bytes limit");
     TEST_UTF8(0x1F642, "\xF0\x9F\x99\x82", 4, "SLIGHTLY SMILING FACE");
+
+    char buffer_utf16[5];
+
+    #define TEST_UTF16_LE(CHAR, STR, RES, COMMENT) \
+        TEST_ENC(CHAR, STR, RES, COMMENT, MJB_ENCODING_UTF_16_LE, buffer_utf16, 5)
+
+    TEST_UTF16_LE(0x007F, "\x7F\x00", 2, "ASCII limit UTF-16LE");
+    TEST_UTF16_LE(0x07FF, "\xFF\x07", 2, "2-bytes limit UTF-16LE");
+    TEST_UTF16_LE(0x1E0A, "\x0A\x1E", 2, "LATIN CAPITAL LETTER D WITH DOT ABOVE UTF-16LE");
+    TEST_UTF16_LE(0xFFFD, "\xFD\xFF", 2, "3-bytes limit UTF-16LE");
+    TEST_UTF16_LE(0x10FFFE, "\xFF\xDB\xFE\xDF", 4, "4-bytes limit UTF-16LE");
+    TEST_UTF16_LE(0x1F642, "\x3D\xD8\x42\xDE", 4, "SLIGHTLY SMILING FACE UTF-16LE");
+
+    #define TEST_UTF16_BE(CHAR, STR, RES, COMMENT) \
+        TEST_ENC(CHAR, STR, RES, COMMENT, MJB_ENCODING_UTF_16_BE, buffer_utf16, 5)
+
+    TEST_UTF16_BE(0x007F, "\x00\x7F", 2, "ASCII limit UTF-16BE");
+    TEST_UTF16_BE(0x07FF, "\x07\xFF", 2, "2-bytes limit UTF-16BE");
+    TEST_UTF16_BE(0x1E0A, "\x1E\x0A", 2, "LATIN CAPITAL LETTER D WITH DOT ABOVE UTF-16BE");
+    TEST_UTF16_BE(0xFFFD, "\xFF\xFD", 2, "3-bytes limit UTF-16BE");
+    TEST_UTF16_BE(0x10FFFE, "\xDB\xFF\xDF\xFE", 4, "4-bytes limit UTF-16BE");
+    TEST_UTF16_BE(0x1F642, "\xD8\x3D\xDE\x42", 4, "SLIGHTLY SMILING FACE UTF-16BE");
 
     /*uint8_t state = MJB_UTF8_ACCEPT;
     mjb_codepoint codepoint;
@@ -46,6 +71,11 @@ void *test_utf8(void *arg) {
     if(state != MJB_UTF8_ACCEPT) {
         printf("The string is not well-formed\n");
     }*/
+
+    #undef TEST_ENC
+    #undef TEST_UTF8
+    #undef TEST_UTF16_LE
+    #undef TEST_UTF16_BE
 
     return NULL;
 }
