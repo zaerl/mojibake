60fec273b1aabc31158561a19ba8b798c61e5f99
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-10-14 17:36:04 +0200

    Fix MSVC bug

---
+++ utils/sqlite3/windows-fixes.c
@@ -0,0 +1,17 @@
+/**
+ * The Mojibake library
+ *
+ * This file is distributed under the MIT License. See LICENSE for details.
+ */
+
+/* Windows fixes */
+#if defined(_MSC_VER)
+/*
+** Remove all nodes that are part of expression pExpr from the rename list.
+*/
+SQLITE_PRIVATE void sqlite3RenameExprUnmap(Parse *pParse, Expr *pExpr) {
+    // Fake function for MSVC compatibility
+
+    return;
+}
+#endif
