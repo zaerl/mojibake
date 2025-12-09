a259bec2e25d104b19380b807926bee4c5052e74
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-12-09 16:09:04 +0100

    Update version

--- src/mojibake.h
+++ src/mojibake.h
@@ -38,15 +38,15 @@ extern "C" {
     _Static_assert(sizeof(char) == 1, "char must be 1 byte");
 #endif
 
-#define MJB_VERSION_NUMBER   0x6 // MAJOR << 8 | MINOR << 4 | REVISION
+#define MJB_VERSION_NUMBER   0x7 // MAJOR << 8 | MINOR << 4 | REVISION
 #define MJB_VERSION_MAJOR    0
 #define MJB_VERSION_MINOR    0
-#define MJB_VERSION_REVISION 6
+#define MJB_VERSION_REVISION 7
 
 #ifdef __EMSCRIPTEN__
-    #define MJB_VERSION "0.0.6-WASM"
+    #define MJB_VERSION "0.0.7-WASM"
 #else
-    #define MJB_VERSION "0.0.6"
+    #define MJB_VERSION "0.0.7"
 #endif
 
 #define MJB_UNICODE_VERSION       "17.0.0"
