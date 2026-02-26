8b71ad3f531cb6cfe685c5c0ee42d855cf914181
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-02-26 15:01:31 +0100

    Update Windows fixes

--- utils/sqlite3/windows-fixes.c
+++ utils/sqlite3/windows-fixes.c
@@ -7,33 +7,12 @@
 /* Windows fixes */
 #if defined(_MSC_VER)
 
-SQLITE_PRIVATE const void *sqlite3RenameTokenMap( Parse *pParse, const void *pPtr,
-    const Token *pToken) {
-    return NULL;
-}
-
-/*
-** Remove all nodes that are part of expression pExpr from the rename list.
-*/
-SQLITE_PRIVATE void sqlite3RenameExprUnmap(Parse *pParse, Expr *pExpr) {
-    return;
-}
-
-SQLITE_PRIVATE void sqlite3RenameExprlistUnmap(Parse *pParse, ExprList *pEList) {
-    return;
-}
-
-SQLITE_PRIVATE void sqlite3RenameTokenRemap(Parse *pParse, const void *pTo, const void *pFrom) {
-    return;
-}
-
-SQLITE_PRIVATE void sqlite3AlterRenameColumn(Parse *pParse, SrcList *pSrc, Token *pOld,
-    Token *pNew) {
-    return;
-}
-
-SQLITE_PRIVATE void sqlite3AlterBeginAddColumn(Parse *pParse, SrcList *pSrc) {
-    return;
-}
+SQLITE_PRIVATE const void *sqlite3RenameTokenMap( Parse *pParse, const void *pPtr, const Token *pToken) { return NULL; }
+SQLITE_PRIVATE void sqlite3RenameExprUnmap(Parse *pParse, Expr *pExpr) { return; }
+SQLITE_PRIVATE void sqlite3RenameExprlistUnmap(Parse *pParse, ExprList *pEList) { return; }
+SQLITE_PRIVATE void sqlite3RenameTokenRemap(Parse *pParse, const void *pTo, const void *pFrom) { return; }
+SQLITE_PRIVATE void sqlite3AlterRenameColumn(Parse *pParse, SrcList *pSrc, Token *pOld, Token *pNew) { return; }
+SQLITE_PRIVATE void sqlite3AlterBeginAddColumn(Parse *pParse, SrcList *pSrc) { return; }
+SQLITE_PRIVATE void sqlite3Reindex(Parse *pParse, Token *pName1, Token *pName2) { return }
 
 #endif
