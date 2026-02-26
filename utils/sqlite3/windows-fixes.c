7a00f7fefc9325684f0951a41ac0988e8173f8d6
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-02-26 14:58:44 +0100

    Update Windows fixes

--- utils/sqlite3/windows-fixes.c
+++ utils/sqlite3/windows-fixes.c
@@ -9,8 +9,6 @@
 
 SQLITE_PRIVATE const void *sqlite3RenameTokenMap( Parse *pParse, const void *pPtr,
     const Token *pToken) {
-    // Fake function for MSVC compatibility
-
     return NULL;
 }
 
@@ -18,27 +16,23 @@ SQLITE_PRIVATE const void *sqlite3RenameTokenMap( Parse *pParse, const void *pPt
 ** Remove all nodes that are part of expression pExpr from the rename list.
 */
 SQLITE_PRIVATE void sqlite3RenameExprUnmap(Parse *pParse, Expr *pExpr) {
-    // Fake function for MSVC compatibility
-
     return;
 }
 
 SQLITE_PRIVATE void sqlite3RenameExprlistUnmap(Parse *pParse, ExprList *pEList) {
-    // Fake function for MSVC compatibility
-
     return;
 }
 
 SQLITE_PRIVATE void sqlite3RenameTokenRemap(Parse *pParse, const void *pTo, const void *pFrom) {
-    // Fake function for MSVC compatibility
-
     return;
 }
 
 SQLITE_PRIVATE void sqlite3AlterRenameColumn(Parse *pParse, SrcList *pSrc, Token *pOld,
     Token *pNew) {
-    // Fake function for MSVC compatibility
+    return;
+}
 
+SQLITE_PRIVATE void sqlite3AlterBeginAddColumn(Parse *pParse, SrcList *pSrc) {
     return;
 }
 
