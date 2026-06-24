058d86647fe5e4be4633707b749cf34cae5bada3
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-06-24 08:50:52 +0200

    Safety checks on codepoint scan

--- src/shell/commands/codepoint.c
+++ src/shell/commands/codepoint.c
@@ -5,18 +5,24 @@
  */
 
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
 
+#include "../../mojibake.h"
 #include "commands.h"
 
 int mjbsh_codepoint_command(int argc, char * const argv[], unsigned int flags) {
     char buffer[5];
+    char *endptr = NULL;
 
-    mjb_codepoint codepoint;
+    errno = 0;
+    unsigned long value = strtoul(argv[0], &endptr, 16);
 
-    if(sscanf(argv[0], "%x", &codepoint) != 1) {
+    if(endptr == argv[0] || *endptr != '\0' || errno == ERANGE || value > MJB_CODEPOINT_MAX) {
         return 1;
     }
 
+    mjb_codepoint codepoint = (mjb_codepoint)value;
     unsigned int length = mjb_codepoint_encode(codepoint, buffer, 5, MJB_ENCODING_UTF_8);
 
     if(length == 0) {
