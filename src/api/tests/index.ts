29067dd1b3ad16d79038b4f7e59184f715487d87
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-07-02 13:33:41 +0200

    Add total counts

--- src/api/tests/index.ts
+++ src/api/tests/index.ts
@@ -7,13 +7,26 @@
 // This is a temporary file for testing the Mojibake API. Do not edit.
 
 import createMojibake from '../index.js';
-import { ATT_ASSERT, att_set_verbose } from './attraction.js';
+import { ATT_ASSERT, att_get_total_tests, att_get_valid_tests, att_set_verbose } from './attraction.js';
 
 // Temporary file, do not edit.
 const mojibake = await createMojibake({
   locateFile: (path, prefix) => `${prefix}${path}`
 });
 
-att_set_verbose(2);
+let showColors = true;
+let verbosity = 0;
+
+att_set_verbose(verbosity);
 
 ATT_ASSERT(mojibake.unicodeVersion(), '17.0.0', 'Unicode version should be 17.0.0');
+
+const valid = att_get_valid_tests();
+const total = att_get_total_tests();
+const isValid = valid === total;
+
+const colorCode = showColors ? (isValid ? "\x1B[32m" : "\x1B[31m") : "";
+
+console.log(
+  `${verbosity >= 1 ? "\n" : ""}Tests valid/run: ${colorCode}${valid}/${total}${showColors ? "\x1B[0m" : ""}`
+);
