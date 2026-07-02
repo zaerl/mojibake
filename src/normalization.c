fb73773c9f56698aedc89f7ec5a9a9c5a6167e99
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-07-02 10:43:03 +0200

    Windows: fix warning C4267 on GItHub

--- src/normalization.c
+++ src/normalization.c
@@ -14,16 +14,16 @@
 static void mjb_normalization_sort(mjb_n_character array[], size_t size) {
     for(size_t step = 1; step < size; ++step) {
         mjb_n_character key = array[step];
-        int j = step - 1;
+        size_t j = step;
 
         // Move elements of array[0..step-1], that are greater than key,
         // to one position ahead of their current position
-        while(j >= 0 && array[j].combining > key.combining) {
-            array[j + 1] = array[j];
-            j = j - 1;
+        while(j > 0 && array[j - 1].combining > key.combining) {
+            array[j] = array[j - 1];
+            --j;
         }
 
-        array[j + 1] = key;
+        array[j] = key;
     }
 }
 
