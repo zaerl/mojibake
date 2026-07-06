73d62e8b5913d3b05e2d0a27640096f6c1f3214e
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-07-07 00:18:24 +0200

    Fix: WASM failing tests

--- src/api/tests/index.ts
+++ src/api/tests/index.ts
@@ -117,8 +117,8 @@ ATT_ASSERT(mojibake.codepointEastAsianWidth(0x20), EastAsianWidth.NARROW,
 ATT_ASSERT(mojibake.displayWidth('Hello'), 5, 'displayWidth');
 ATT_ASSERT(mojibake.localeParse('sr-Latn-RS').region, 'RS', 'localeParse');
 ATT_ASSERT(mojibake.localeSet(Locale.EN), true, 'localeSet');
-ATT_ASSERT(mojibake.version(), '0.2.2-WASM', 'version');
-ATT_ASSERT(mojibake.versionNumber(), 34, 'versionNumber');
+ATT_ASSERT(mojibake.version(), '0.2.3-WASM', 'version');
+ATT_ASSERT(mojibake.versionNumber(), 0x23, 'versionNumber');
 ATT_ASSERT(mojibake.unicodeVersion(), '17.0.0', 'unicodeVersion');
 
 const valid = att_get_valid_tests();
