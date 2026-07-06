246304bc9a774df3f5b39b5b226ebb912e6b91c1
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2026-07-06 15:18:35 +0200

    Windows: fix warning C4232

--- src/mojibake.c
+++ src/mojibake.c
@@ -9,8 +9,33 @@
 
 #include "mojibake-internal.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+static void *mjb_default_alloc(size_t size) {
+    return malloc(size);
+}
+
+static void *mjb_default_realloc(void *ptr, size_t new_size) {
+    return realloc(ptr, new_size);
+}
+
+static void mjb_default_free(void *ptr) {
+    free(ptr);
+}
+
+// warning C4232: nonstandard extension used: 'memory_alloc': address of dllimport 'malloc' is not
+// static, identity not guaranteed
+#define MJB_DEFAULT_ALLOC mjb_default_alloc
+#define MJB_DEFAULT_REALLOC mjb_default_realloc
+#define MJB_DEFAULT_FREE mjb_default_free
+#else
+#define MJB_DEFAULT_ALLOC malloc
+#define MJB_DEFAULT_REALLOC realloc
+#define MJB_DEFAULT_FREE free
+#endif
+
 // Default allocators are valid before any explicit memory-function override.
-mojibake mjb_global = { false, malloc, realloc, free, MJB_LOCALE_EN };
+mojibake mjb_global = { false, MJB_DEFAULT_ALLOC, MJB_DEFAULT_REALLOC, MJB_DEFAULT_FREE,
+    MJB_LOCALE_EN };
 
 // Set the library memory functions.
 MJB_EXPORT mjb_status mjb_set_memory_functions(mjb_alloc_fn alloc_fn, mjb_realloc_fn realloc_fn,
@@ -20,15 +45,15 @@ MJB_EXPORT mjb_status mjb_set_memory_functions(mjb_alloc_fn alloc_fn, mjb_reallo
     }
 
     if(alloc_fn == NULL) {
-        alloc_fn = malloc;
+        alloc_fn = MJB_DEFAULT_ALLOC;
     }
 
     if(realloc_fn == NULL) {
-        realloc_fn = realloc;
+        realloc_fn = MJB_DEFAULT_REALLOC;
     }
 
     if(free_fn == NULL) {
-        free_fn = free;
+        free_fn = MJB_DEFAULT_FREE;
     }
 
     mjb_global.memory_alloc = alloc_fn;
@@ -41,9 +66,9 @@ MJB_EXPORT mjb_status mjb_set_memory_functions(mjb_alloc_fn alloc_fn, mjb_reallo
 
 MJB_EXPORT void mjb_shutdown(void) {
     mjb_global.memory_functions_locked = false;
-    mjb_global.memory_free = free;
-    mjb_global.memory_realloc = realloc;
-    mjb_global.memory_alloc = malloc;
+    mjb_global.memory_free = MJB_DEFAULT_FREE;
+    mjb_global.memory_realloc = MJB_DEFAULT_REALLOC;
+    mjb_global.memory_alloc = MJB_DEFAULT_ALLOC;
     mjb_global.locale = MJB_LOCALE_EN;
 }
 
